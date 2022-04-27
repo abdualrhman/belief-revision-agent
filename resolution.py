4d7378dca27ef019b6c7482f697ae6c14b1746f2
Abdulrahman Ramadan <abdulrahman.ramdan@outlook.com>
2022-04-27 09:45:05 +0200

    implement entaiment

---
+++ resolution.py
@@ -0,0 +1,84 @@
+def remove_all(item, seq):
+    if isinstance(seq, str):
+        return seq.replace(item, '')
+    elif isinstance(seq, set):
+        rest = seq.copy()
+        rest.remove(item)
+        return rest
+    else:
+        return [x for x in seq if x != item]
+def unique(seq):
+    return list(set(seq))
+
+def disjuncts(clause):
+    return dissociate(Or, [clause])
+
+def conjuncts(clause):
+    return dissociate(And, [clause])
+
+def associate(op, args):
+    args = dissociate(op, args)
+    if len(args) == 0:
+        return op.identity
+    elif len(args) == 1:
+        return args[0]
+    else:
+        return op(*args)
+
+def dissociate(op, args):
+    result = []
+    print(args, op)
+    def collect(subargs):
+        for arg in subargs:
+            if isinstance(arg, op):
+                collect(arg.args)
+            else:
+                result.append(arg)
+
+    collect(args)
+    return result
+
+
+
+
+def pl_resolve(ci, cj):
+    """Return all clauses that can be obtained by resolving clauses ci and cj."""
+    clauses = []
+    for di in disjuncts(ci):
+        for dj in disjuncts(cj):
+            if di == ~dj or ~di == dj:
+                clauses.append(associate(Or, unique(remove_all(di, disjuncts(ci)) + remove_all(dj, disjuncts(cj)))))
+    return clauses
+
+def pl_resolution(kb, alpha):
+    """
+    [Figure 7.12]
+    Propositional-logic resolution: say if alpha follows from KB.
+    >>> pl_resolution(horn_clauses_KB, A)
+    True
+    """
+    alpha = to_cnf(alpha)
+    clauses = []
+    for c in kb:
+        clauses += conjuncts(c)
+        
+    clauses+=conjuncts(to_cnf(~alpha))
+    if False in clauses:
+        return True
+    new = set()
+    while True:
+        n = len(clauses)
+        pairs = [(clauses[i], clauses[j])
+                 for i in range(n) for j in range(i + 1, n)]
+        for (ci, cj) in pairs:
+            # print(type(ci), type(cj))
+            resolvents = pl_resolve(ci, cj)
+            # contain emepty clause 
+            if False in resolvents:
+                return True
+            new = new.union(set(resolvents))
+        if new.issubset(set(clauses)):
+            return False 
+        for c in new:
+            if c not in clauses:
+                clauses.append(c)
